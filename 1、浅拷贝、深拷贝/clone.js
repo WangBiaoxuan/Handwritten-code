abe0dec247774347cb676ff39188b2250ec2df4b
wangbiaoxuan <>
2021-08-18 09:53:08 +0800

    feat: 增加深拷贝\浅拷贝

---
+++ 1、浅拷贝、深拷贝/clone.js
@@ -0,0 +1,27 @@
+// 浅拷贝
+function clone (parent, child = {}) {
+//    return Object.assign(child, parent)
+//    return { ...child, ...parent }
+    for (let i in parent) {
+        if (parent.hasOwnPorperty('i')) {
+            child[i] = parent[i]
+        }
+    }
+    return child;
+}
+
+
+// 深拷贝
+function deepClone(parent, child = {}) {
+    for (let i in parent) {
+        if (typeof parent[i] === 'object') {
+            // 如果parent是数组, child应该返回也是数组，而不是对象
+            let childDefaultValue = Object.prototype.toString.call(parent[i]) === '[object Array]' ? [] : {};
+            child[i] = deepClone(parent[i], childDefaultValue)
+        } else {
+            child[i] = parent[i];
+        }
+    }
+
+    return child;
+}
