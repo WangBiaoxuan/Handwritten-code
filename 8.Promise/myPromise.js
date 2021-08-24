9b3f00c11891f34caf6c138272de082a628e1d1b
wangbiaoxuan <>
2021-08-24 08:07:13 +0800

    feat: 完善Promise

--- 8.Promise/myPromise.js
+++ 8.Promise/myPromise.js
@@ -66,24 +66,36 @@ class MyPromise {
     }
 
     then(function(onFullfill, onReject) {
-
-        return new MyPromise(function(onResolveNext, onRejectNext){
+        return new MyPromise(function(resolveNext, rejectNext){
 
             const resolveFn = function(value) {
                 try {
                     const res = onFullfill(value)
                     if (res instanceof MyPromise) {
-                        res.then(onResolveNext, onRejectNext)
+                        // 把resolveNext、rejectNext作为res的then参数, 有可能添加到res的_fullFilledCallback/_rejectedCallback回调当中
+                        // 前一个Promise 状态改变后, 调用后一个 Promise的resolveNext/rejectNext, 触发通过then添加到后一个Promise的_fullFilledCallback/_rejectedCallback回调中的onFullfillNext/onRejectNext执行
+                        res.then(resolveNext, rejectNext)
+                    } else {
+                        // 调用后一个 Promise的resolveNext, 触发通过then添加到后一个Promise的_fullFilledCallback回调中的onFullfillNext执行
+                        resolveNext(res);
                     }
                 } catch(error) {
-                    onRejectNext(error);
+                    // 调用后一个 Promise的rejectNext, 触发通过then添加到后一个Promise的_rejectedCallback回调中的onRejectNext执行
+                    rejectNext(error);
                 }
-
-                
             }
 
-            const rejectFn = function(value) {
-                
+            const rejectFn = function(reason) {
+                try {
+                    const res = onReject(reason)
+                    if (res instanceof MyPromise) {
+                        res.then(resolveNext, rejectNext)
+                    } else {
+                        resolveNext(res);
+                    }
+                } catch(error) {
+                    rejectNext(error);
+                }
             }
             
 
@@ -93,16 +105,109 @@ class MyPromise {
                     this._fullFilledCallback.push(rejectFn);
                     break;
                 case 'FULLFILED':
-
-
+                    resolveFn(this._value);
+                    break;
                 case 'REJECTED':
-
+                    rejectFn(this._value);
+                    break;
             }
-
-
-
         })
-        
-    });
+    })
+
+    catch(onRejectFn) {
+        return this.then(() => {}, (error) => {
+            onRejectFn(error)
+        })
+    }
+
+    finally(finallyFn) {
+        return this.then((value) => {
+            finallyFn(value)
+        }, (error) => {
+            finallyFn(error)
+        })
+    }
+
+    // resolve的作用: https://developer.mozilla.org/zh-CN/docs/Web/JavaScript/Reference/Global_Objects/Promise/resolve
+    static resolve(value) {
+        // 这个很关键，因为在all中有使用, 如果是Promise就返回Promise
+        if (value instanceof MyPromise) return value;
+        return new MyPromise((resolve, reject) => { resolve(value); })
+    }
+
+    // reject的作用: https://developer.mozilla.org/zh-CN/docs/Web/JavaScript/Reference/Global_Objects/Promise/reject
+    static reject(reason) {
+        if (value instanceof MyPromise) return value;
+        return new MyPromise((resolve, reject) => { reject(reason); })
+    }
+
+    static all(array) {
+        return new MyPromise((resolve, reject) => {
+            const res = [];
+            let count = 0;
+            array.forEach((item) => {
+
+                MyPromise.resolve(item).then((value) => {
+                    count++;
+                    res.push(value)
+
+                    if (count === array.length) {
+                        resolve(res);
+                    }
+                }, (reason) => {
+                    reject(reason)
+                })
+            });
+        })
+    }
+
+    static allSettled(array) {
+        return new MyPromise((resolve, reject) => {
+            const res = [];
+            let count = 0;
+            array.forEach((item, index) => {
+                MyPromise.resolve(item).then((value) => {
+                    count++;
+                    res[index] = value;
+                    if (count === array.length) {
+                        resolve(res);
+                    }
+                }, (reason) => {
+                    count++;
+                    res[index] = reason;
+                    if (count === array.length) {
+                        resolve(res);
+                    }
+                })
+            });
+        })
+    }
+
+    static race(array) {
+        return new MyPromise((resolve, reject) => {
+            array.forEach((item) => {
+                MyPromise.resolve(item).then((value) => {
+                    resolve(value);
+                }, (reason) => {
+                    reject(reason)
+                })
+            })
+        })
+    }
+
+    static any(array) {
+        return new MyPromise((resolve, reject) => {
+            let count = 0;
+            array.forEach((item) => {
+                MyPromise.resolve(item).then((value) => {
+                    resolve(value);
+                }, (reason) => {
+                    count++;
+                    if (count === array.length)
+                    reject(reason);
+                })
+            })
+        })
+    }
 }
 
